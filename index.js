50c8fea933ed212cb24a1d772ff836adafaf54f4
Gonzalo <102692082+gnz6@users.noreply.github.com>
2022-09-25 18:03:34 -0300

    funcion agragada

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function laCajaDePandora(numero) {
-  // proximamente escribiremos codigo aqui
+
+  if (numero%2 === 0){
+    return numero.toString(2)
+  }else{
+    return numero.toString(16)
+  }
+  
 }
 
 function paulaCelman() {
