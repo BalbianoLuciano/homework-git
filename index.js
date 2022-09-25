bf69031862e8377629eb175a12ebd564bae00404
Sebastian Diaz <sediaz84@gmail.com>
2022-09-25 18:10:49 -0300

    funcion nueva

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+    if(numero%2 === 0){
+      let resultado =  numero.toString(2)
+      return resultado;
+      } else {
+        let resultado2 = numero.toString(16)
+         return resultado2;
+
+      }
     }
