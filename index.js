6a280ced0789442c7a66f726cbd17ec86a547b03
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-09-25 17:50:39 -0300

    PCV

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
-    }
+function laCajaDePandora(numero) {
+  // proximamente escribiremos codigo aqui
+}
+
+function paulaCelman() {
+  let paulaCelman = {
+    nombre: "Paula Celman",
+    edad: 41,
+    nacionalidad: "Argentina",
+  };
+  return paulaCelman;
+}
