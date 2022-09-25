b0a1f192ee1c0bb62b931cd637f889d29bd63c8e
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-09-25 18:14:30 -0300

    conflicto

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 function laCajaDePandora(numero) {
-  // proximamente escribiremos codigo aqui
-  if(numero%2 === 0){
-    numero.toString(2)
-} else {
-    
-}
+    if(numero%2 === 0){
+        numero.toString(2)
+    } else{
+        numero.toString(16)
+    }
 }
 
 function paulaCelman() {
